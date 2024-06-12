12d14d18e3e02b5408b29b5a4c65790e66844fe2
lightingguan <gyd3532@qq.com>
2024-06-12 20:05:34 +0800

    second commit

--- conda_create_environment.sh
+++ conda_create_environment.sh
@@ -1,14 +1,12 @@
 conda create -y -n cross_task python==3.9
 conda activate cross_task
-conda install -y numba
+conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1 -c pytorch -c conda-forge
 pip install librosa
-pip install ffmpeg
 pip install pandas==1.4.4
 pip install h5py
-pip install  scipy
-conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1 -c pytorch -c conda-forge
-pip install torch==1.13.1
 pip install pytorch-lightning==1.5.0
+pip install torch==1.13.1
+conda install pytorch==1.8.0 -c pytorch
 pip install asteroid==0.4.1
 pip install psds_eval
 pip install sed_eval==0.2.1
